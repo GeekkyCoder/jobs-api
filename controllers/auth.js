8523445b6f87e85ade73e70ca6cdbd2b30cf21f5
Faraz <farazatgithub@gmail.com>
2023-04-08 18:16:48 +0500

    created user and hashed password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,35 @@
-const register = async (req,res) => {
-    res.send("register user")
-}
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+const bcrypt = require("bcryptjs");
 
-const login = async (req,res) => {
-    res.send("login user")
-}
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  console.log(hashedPassword);
+
+  const tempUser = {
+    name,
+    email,
+    password:hashedPassword,
+  };
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("plz provide name,email and password");
+  }
+
+  const user = await User.create(tempUser);
+  res.status(StatusCodes.CREATED).json({ user });
+};
+
+const login = async (req, res) => {
+  res.send("login user");
+};
 
 module.exports = {
-    register,
-    login,
-}
+  register,
+  login,
+};
