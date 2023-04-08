9a6b2865eb5ec80f76ab4f7f97d4063c2738efaa
Faraz <farazatgithub@gmail.com>
2023-04-08 19:47:30 +0500

    register functionaltiy completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,20 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError } = require("../errors");
-const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  console.log(hashedPassword);
-
-  const tempUser = {
-    name,
-    email,
-    password:hashedPassword,
-  };
 
   if (!name || !email || !password) {
     throw new BadRequestError("plz provide name,email and password");
   }
 
-  const user = await User.create(tempUser);
-  res.status(StatusCodes.CREATED).json({ user });
+  const user = await User.create({ name, email, password });
+
+  const token = user.createJWT()
+
+  res.status(StatusCodes.CREATED).json({ user:{name:user.name}, token });
 };
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,11 +25,16 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", async function(next){
+// mongoose middleware docs 
+//  runs before the doc is saved into database , this refers to the document here!
+userSchema.pre("save", async function(){
    const salt = await bcrypt.genSalt(10)
    const hashedPassword = await bcrypt.hash(this.password,salt)
-   this.password = hashedPassword
-   next()
+   this.password = hashedPassword 
 })
+
+userSchema.methods.createJWT = function (){
+  return jwt.sign({userId:this._id,name:this.name},process.env.SECRET_KEY,{expiresIn:process.env.JWT_LIFETIME})
+}
 
 module.exports = mongoose.model("user", userSchema);
