25cae018e6d549d56d900575ec10c6a243714bec
Faraz <farazatgithub@gmail.com>
2023-04-11 14:53:57 +0500

    crud operation completed and fixed some error logics

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,24 +1,61 @@
+const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
+const { NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  const user = req.user
-  res.status(StatusCodes.OK).json({user});
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs });
 };
 
 const getJob = async (req, res) => {
-  res.send("get a single job");
+  const jobId = req.params.id;
+  const userId = req.user.userId;
+  const job = await Job.findOne({ createdBy: userId, _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`no job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
-  res.send("creating a job");
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const updateJob = async (req, res) => {
-  res.send("update a job");
+  const jobId = req.params.id;
+  const userId = req.user.userId;
+  const payload = req.body;
+  const updatedJob = await Job.findOneAndUpdate(
+    { _id: jobId, createdBy: userId },
+    payload,
+    { upsert: true }
+  );
+
+  if (!updatedJob) {
+    throw new NotFoundError(`job with ${jobId} does not exist`);
+  }
+
+  const job = await Job.findOne({ _id: updatedJob._id });
+
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const deleteJob = async (req, res) => {
-  res.send("delete a job");
+  const jobId = req.params.id;
+  const userId = req.user.userId;
+  const job = await Job.findOneAndDelete({ _id: jobId, createdBy: userId });
+
+  if (!job) {
+    throw new NotFoundError(`job with id ${jobId} does not exist`);
+  }
+
+  res
+    .status(StatusCodes.OK)
+    .json(`successfully deleted the job with id ${jobId}`);
 };
 
 module.exports = {
